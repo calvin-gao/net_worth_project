b1dbdba8b102caad380dac795088b456be53d096
Brian Chiu <bhychiu89@yahoo.com>
2022-04-15 23:14:41 -0700

    added vue routers

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import HelloWorld from './components/HelloWorld.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: HelloWorld },
+    // { path: '/about', component: About },
+  ]
+  
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
+
+createApp(App).use(router).mount('#app')

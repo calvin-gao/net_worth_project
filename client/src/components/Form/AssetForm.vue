25792eccd2b688bf82a443b2b0808ef19f42f5a4
Brian Chiu <bhychiu89@yahoo.com>
2022-04-19 02:57:14 -0700

    add simple validator

--- client/src/components/Form/AssetForm.vue
+++ client/src/components/Form/AssetForm.vue
@@ -2,27 +2,47 @@
 <form action="" class="form-inline" ref="form" @submit="addAssets">
         <div class="form-group">
             <label>Asset Name: </label>
-            <input type="text" class="form-control" v-model="name" placeholder="Name" />
+            <input type="text" class="form-control" v-model="state.name" placeholder="Name" />
         </div>
         <div class="form-group">
             <label>Amount: </label>    
-            <input type="text" class="form-control" v-model.number="amount" placeholder="Amount" />   
+            <input type="text" class="form-control" v-model="state.amount" placeholder="Amount" />   
         </div>
+        <p v-for="error of v.$errors" :key="error.$uid">
+            {{ error.$property }} - {{ error.$message }}
+        </p>
     <button type="submit" class="btn btn-default">Submit</button>
 </form>
 </template>
 
 <script setup>
 import { Asset } from '../../assets/helper/constants.js';
-import { ref, defineEmits } from 'vue';
+import { defineEmits, reactive, computed } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { required, numeric } from '@vuelidate/validators';
 
 const emit = defineEmits('add-assets');
 
-const name = ref('');
-const amount = ref(0);
+const state = reactive({
+    name: '',
+    amount: 0,
+});
+
+const rules = computed(() => {
+    return {
+        name: {required},
+        amount: {required, numeric},
+    }
+});
+
+const v = useVuelidate(rules, state, { $autoDirty: true });
 
 const addAssets = (event) => {
     event.preventDefault();
-    emit('add-assets', new Asset(name.value, amount.value));
+    if (v.value.$invalid) {
+        return;
+    }
+    emit('add-assets', new Asset(state.name, parseFloat(state.amount)));
 }
+
 </script>

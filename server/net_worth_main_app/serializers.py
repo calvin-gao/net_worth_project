b46253776be24ede7462acf55680c9f76bebdd33
Brian Chiu <bhychiu89@yahoo.com>
2022-04-25 23:16:23 -0700

    add serializer change

--- server/net_worth_main_app/serializers.py
+++ server/net_worth_main_app/serializers.py
@@ -1,36 +1,8 @@
+from asyncore import read
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from .models import Asset
-
-class CreateUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'],
-                                        None,
-                                        validated_data['password'])
-        return user
-
-class UserSerializer(serializers.ModelSerializer):
-    assets = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'assets')
-
-class LoginUserSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Invalid Details.")
     
 class AssetSerializer(serializers.ModelSerializer):
     
@@ -50,3 +22,32 @@ class AssetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asset
         fields = ('id', 'name', 'amount')
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'],
+                                        None,
+                                        validated_data['password'])
+        return user
+        
+class UserSerializer(serializers.ModelSerializer):
+    assets = AssetSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'assets')
+
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid Details.")
